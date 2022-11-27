8ce96fad0d560e31c0c4d8568ef7abb66154ff6b
E-Sahra-B <bendenizsahra@gmail.com>
2022-11-27 03:28:27 +0300

    .

--- 4.HaftaOdev/kontrol.php
+++ 4.HaftaOdev/kontrol.php
@@ -7,31 +7,39 @@ class TcKontrol
         $durum = $_POST["durum"];
 
         if (strlen($tc) == 11) {
-            // $sonuc = filter_input(INPUT_POST, $tc, FILTER_SANITIZE_NUMBER_INT);
+            //$sonuc = filter_input(INPUT_POST, $tc, FILTER_SANITIZE_NUMBER_INT);
+            //$sonuc = filter_var($this->$tc,FILTER_VALIDATE_INT)
             // if ($sonuc == true) {
-            if ($tc[0] != 0) {
-                $tc10Rakam = ((($tc[0] + $tc[2] + $tc[4] + $tc[6] + $tc[8]) * 7) - ($tc[1] + $tc[3] + $tc[5] + $tc[7])) % 10;
-                if ($tc10Rakam == $tc[9]) {
-                    $tc11Rakam = ($tc[0] + $tc[1] + $tc[2] + $tc[3] + $tc[4] + $tc[5] + $tc[6] + $tc[7] + $tc[8] + $tc[9]) % 10;
-                    if ($tc11Rakam == $tc[10]) {
-                        $durum = "TC Kimlik Geçerli";
-                        return $durum;
+
+            //$sonuc = preg_replace("/[^0-9]/", "", $tc);
+            // if ($sonuc == true) {
+
+            // if (is_numeric($tc)) {
+
+            if (ctype_digit($tc)) {
+                if ($tc[0] != 0) {
+                    $tc10Rakam = ((($tc[0] + $tc[2] + $tc[4] + $tc[6] + $tc[8]) * 7) - ($tc[1] + $tc[3] + $tc[5] + $tc[7])) % 10;
+                    if ($tc10Rakam == $tc[9]) {
+                        $tc11Rakam = ($tc[0] + $tc[1] + $tc[2] + $tc[3] + $tc[4] + $tc[5] + $tc[6] + $tc[7] + $tc[8] + $tc[9]) % 10;
+                        if ($tc11Rakam == $tc[10]) {
+                            $durum = "TC Kimlik Geçerli";
+                            return $durum;
+                        } else {
+                            $durum = "TC Kimlik Geçersiz 11. rakam hatalı";
+                            return $durum;
+                        }
                     } else {
-                        $durum = "TC Kimlik Geçersiz 11. rakam hatalı";
+                        $durum = "TC Kimlik Geçersiz 10. rakam hatalı";
                         return $durum;
                     }
                 } else {
-                    $durum = "TC Kimlik Geçersiz 10. rakam hatalı";
+                    $durum = "TC kimlik Geçersiz 0 ile başlayamaz";
                     return $durum;
                 }
             } else {
-                $durum = "TC kimlik Geçersiz 0 ile başlayamaz";
+                $durum = "TC Kimlik Geçersiz TC Kimlik Numarası yalnızca rakamlardan oluşmaktadır.";
                 return $durum;
             }
-            // } else {
-            //     $durum = "TC Kimlik Geçersiz TC Kimlik Numarası yalnızca rakamlardan oluşmaktadır.";
-            //     return $durum;
-            // }
         } else {
             $durum = "TC Kimlik Geçersiz 11 hane olmak zorundadır.";
             return $durum;
