6191b8f9f976e6885d8add1e27e23950e5100cd3
E-Sahra-B <bendenizsahra@gmail.com>
2023-01-22 19:22:21 +0300

    UrunAdminCrudIslemleri

--- E-Ticaret_2/admin/netting/islem.php
+++ E-Ticaret_2/admin/netting/islem.php
@@ -503,3 +503,78 @@ if ($_GET['kategorisil'] == "ok") {
 		Header("Location:../production/kategori.php?sil=no");
 	}
 }
+
+if ($_GET['urunsil'] == "ok") {
+	$sil = $db->prepare("DELETE from urun where urun_id=:urun_id");
+	$kontrol = $sil->execute(array(
+		'urun_id' => $_GET['urun_id']
+	));
+	if ($kontrol) {
+		Header("Location:../production/urun.php?sil=ok");
+	} else {
+		Header("Location:../production/urun.php?sil=no");
+	}
+}
+
+if (isset($_POST['urunekle'])) {
+	$urun_seourl = seo($_POST['urun_ad']);
+	$kaydet = $db->prepare("INSERT INTO urun SET
+		kategori_id=:kategori_id,
+		urun_ad=:urun_ad,
+		urun_detay=:urun_detay,
+		urun_fiyat=:urun_fiyat,
+		urun_video=:urun_video,
+		urun_keyword=:urun_keyword,
+		urun_durum=:urun_durum,
+		urun_stok=:urun_stok,	
+		urun_seourl=:seourl		
+		");
+	$insert = $kaydet->execute(array(
+		'kategori_id' => $_POST['kategori_id'],
+		'urun_ad' => $_POST['urun_ad'],
+		'urun_detay' => $_POST['urun_detay'],
+		'urun_fiyat' => $_POST['urun_fiyat'],
+		'urun_video' => $_POST['urun_video'],
+		'urun_keyword' => $_POST['urun_keyword'],
+		'urun_durum' => $_POST['urun_durum'],
+		'urun_stok' => $_POST['urun_stok'],
+		'seourl' => $urun_seourl
+	));
+	if ($insert) {
+		Header("Location:../production/urun-ekle.php?durum=ok");
+	} else {
+		Header("Location:../production/urun-ekle.php?durum=no");
+	}
+}
+
+if (isset($_POST['urunduzenle'])) {
+	$urun_id = $_POST['urun_id'];
+	$urun_seourl = seo($_POST['urun_ad']);
+	$kaydet = $db->prepare("UPDATE urun SET
+		kategori_id=:kategori_id,
+		urun_ad=:urun_ad,
+		urun_detay=:urun_detay,
+		urun_fiyat=:urun_fiyat,
+		urun_video=:urun_video,
+		urun_keyword=:urun_keyword,
+		urun_durum=:urun_durum,
+		urun_stok=:urun_stok,	
+		urun_seourl=:seourl		
+		WHERE urun_id={$_POST['urun_id']}");
+	$update = $kaydet->execute(array(
+		'kategori_id' => $_POST['kategori_id'],
+		'urun_ad' => $_POST['urun_ad'],
+		'urun_detay' => $_POST['urun_detay'],
+		'urun_fiyat' => $_POST['urun_fiyat'],
+		'urun_video' => $_POST['urun_video'],
+		'urun_keyword' => $_POST['urun_keyword'],
+		'urun_durum' => $_POST['urun_durum'],
+		'urun_stok' => $_POST['urun_stok'],
+		'seourl' => $urun_seourl
+	));
+	if ($update) {
+		Header("Location:../production/urun-duzenle.php?durum=ok&urun_id=$urun_id");
+	} else {
+		Header("Location:../production/urun-duzenle.php?durum=no&urun_id=$urun_id");
+	}
+}
---
+++ E-Ticaret_2/admin/production/urun.php
@@ -0,0 +1,91 @@
+<?php
+$title = "Ürün Listeleme";
+require_once 'header.php';
+$urunsor = $db->prepare("SELECT * FROM urun order by urun_id DESC");
+$urunsor->execute();
+?>
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="clearfix"></div>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <div>
+              <?php
+              if ($_GET['sil'] == "ok") { ?>
+                <div class="alert alert-success">İşlem Başarılı</div>
+              <?php } elseif ($_GET['sil'] == "no") { ?>
+                <div class="alert alert-danger">İşlem Başarısız</div>
+              <?php }
+              ?>
+            </div>
+            <h2>Ürün Listeleme</h2>
+            <div class="clearfix"></div>
+            <div align="right">
+              <a href="urun-ekle.php"><button class="btn btn-success btn-xs"> Yeni Ekle</button></a>
+            </div>
+          </div>
+          <div class="x_content">
+            <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+              <thead>
+                <tr>
+                  <th>Ürün Ad</th>
+                  <th>Kategori Id</th>
+                  <th>Ürün Stok</th>
+                  <th>Ürün Fiyat</th>
+                  <th>Durum</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                while ($uruncek = $urunsor->fetch(PDO::FETCH_ASSOC)) { ?>
+                  <tr>
+                    <td><?php echo $uruncek['urun_ad'] ?></td>
+                    <td>
+                      <!-- Kategori Id'ye Göre Kategori Ad Getirme -->
+                      <?php
+                      $urun_id = $uruncek['kategori_id'];
+                      $kategorisor = $db->prepare("select * from kategori where kategori_id=:id");
+                      $kategorisor->execute(array(
+                        'id' => $urun_id
+                      ));
+                      $kategoricek = $kategorisor->fetch(PDO::FETCH_ASSOC);
+                      echo $kategoricek['kategori_ad'];
+                      ?>
+                      <!-- Kategori Id'ye Göre Kategori Ad Getirme Bitiş -->
+                    </td>
+                    <td><?php echo $uruncek['urun_stok'] ?></td>
+                    <td><?php echo number_format($uruncek['urun_fiyat'], 2, ',', '.') ?></td>
+
+                    <td>
+                      <center><?php
+                              if ($uruncek['urun_durum'] == 1) { ?>
+                          <button class="btn btn-success btn-xs">Aktif</button>
+                        <?php } else { ?>
+                          <button class="btn btn-secondary btn-xs">Pasif</button>
+                        <?php } ?>
+                      </center>
+                    </td>
+                    <td>
+                      <center><a href="urun-duzenle.php?urun_id=<?php echo $uruncek['urun_id']; ?>"><button class="btn btn-primary btn-xs">Düzenle</button></a></center>
+                    </td>
+                    <td>
+                      <center><a href="../netting/islem.php?urun_id=<?php echo $uruncek['urun_id']; ?>&urunsil=ok"><button class="btn btn-danger btn-xs">Sil</button></a></center>
+                    </td>
+                  </tr>
+                <?php  }
+                ?>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /page content -->
+<?php require_once 'footer.php'; ?>
