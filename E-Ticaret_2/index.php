cb75587731a036a96c844b01c43f5f2f00148f8f
E-Sahra-B <bendenizsahra@gmail.com>
2023-01-23 04:33:35 +0300

    AnasayfaHakkimizdaOzet

--- E-Ticaret_2/index.php
+++ E-Ticaret_2/index.php
@@ -50,9 +50,15 @@ require_once 'header.php';
 				<div class="title">Hakkımızda Bilgi</div>
 			</div>
 			<p class="ct">
-
+				<?php
+				$hakkimizdasor = $db->prepare("SELECT * FROM hakkimizda where hakkimizda_id=:id");
+				$hakkimizdasor->execute(array(
+					'id' => 0
+				));
+				$hakkimizdacek = $hakkimizdasor->fetch(PDO::FETCH_ASSOC);
+				echo substr($hakkimizdacek['hakkimizda_icerik'], 0, 1000) ?>
 			</p>
-			<a href="hakkimizda" class="btn btn-default btn-red btn-sm">Devamını Oku</a>
+			<a href="hakkimizda" class="btn btn-default btn-info btn-sm">Devamını Oku</a>
 			<div class="title-bg">
 				<div class="title">Lastest Products</div>
 			</div>
