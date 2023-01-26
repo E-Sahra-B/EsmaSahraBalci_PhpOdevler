9a0a3fbbd21192984c9afdd236d7df4a65dd731b
E-Sahra-B <bendenizsahra@gmail.com>
2023-01-26 21:35:27 +0300

    PhpMaillerKayit

--- E-Ticaret_2/admin/netting/islem.php
+++ E-Ticaret_2/admin/netting/islem.php
@@ -981,3 +981,23 @@ if (isset($_POST['bankasiparisekle'])) {
 		Header("Location:../../odeme.php?durum=no");
 	}
 }
+
+if (isset($_POST['mailayarkaydet'])) {
+	$ayarkaydet = $db->prepare("UPDATE ayar SET
+		ayar_smtphost=:smtphost,
+		ayar_smtpuser=:smtpuser,
+		ayar_smtppassword=:smtppassword,
+		ayar_smtpport=:smtpport
+		WHERE ayar_id=0");
+	$update = $ayarkaydet->execute(array(
+		'smtphost' => $_POST['ayar_smtphost'],
+		'smtpuser' => $_POST['ayar_smtpuser'],
+		'smtppassword' => $_POST['ayar_smtppassword'],
+		'smtpport' => $_POST['ayar_smtpport']
+	));
+	if ($update) {
+		Header("Location:../production/mail-ayar.php?durum=ok");
+	} else {
+		Header("Location:../production/mail-ayar.php?durum=no");
+	}
+}
