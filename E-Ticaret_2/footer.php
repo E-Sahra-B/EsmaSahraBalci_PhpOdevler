551a5ca9c6aca44fd1a18f7becbf2b0ea000211d
E-Sahra-B <bendenizsahra@gmail.com>
2023-01-27 22:16:13 +0300

    FooterDuzenleme

--- E-Ticaret_2/footer.php
+++ E-Ticaret_2/footer.php
@@ -26,17 +26,17 @@
 <div class="f-widget"><!--footer Widget-->
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4"><!--footer twitter widget-->
+			<!--<div class="col-md-4">footer twitter widget
 				<div class="title-widget-bg">
 					<div class="title-widget">Twitter Updates</div>
 				</div>
 				<ul class="tweets">
 					<li>Check out this great #themeforest item for you
-						'Simpler Landing' <a href="#">http://t.co/LbLwldb6 </a>
+						'Simpler Landing' <a href="#"></a>
 						<span>2 hours ago</span>
 					</li>
 					<li class="lastone">Check out this great #themeforest item for you
-						'Simpler Landing' <a href="#">http://t.co/LbLwldb6 </a>
+						'Simpler Landing' <a href="#"> </a>
 						<span>2 hours ago</span>
 					</li>
 				</ul>
@@ -44,25 +44,8 @@
 				<a href="#" class="btn btn-default btn-follow"><i class="fa fa-twitter fa-2x"></i>
 					<div>Follow us on twitter</div>
 				</a>
-			</div><!--footer twitter widget-->
-			<div class="col-md-4"><!--footer newsletter widget-->
-				<div class="title-widget-bg">
-					<div class="title-widget">Newsletter Signup</div>
-				</div>
-				<div class="newsletter">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-					</p>
-					<form role="form">
-						<div class="form-group">
-							<label>Your Email address</label>
-							<input type="email" class="form-control newstler-input" id="exampleInputEmail1" placeholder="Enter email">
-							<button class="btn btn-default btn-red btn-sm">Sign Up</button>
-						</div>
-					</form>
-				</div>
-			</div><!--footer newsletter widget-->
-			<div class="col-md-4"><!--footer contact widget-->
+			</div>footer twitter widget-->
+			<div class="col-md-6"><!--footer contact widget-->
 				<div class="title-widget-bg">
 					<div class="title-widget-cursive">Esb-Ticaret</div>
 				</div>
@@ -72,6 +55,23 @@
 					<li class="fmail lastone"><?php echo $ayarcek['ayar_mail']; ?></li>
 				</ul>
 			</div><!--footer contact widget-->
+			<div class="col-md-6"><!--footer newsletter widget-->
+				<div class="title-widget-bg">
+					<div class="title-widget">Mail Bültenimize Üye Olun</div>
+				</div>
+				<div class="newsletter">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<form role="form">
+						<div class="form-group">
+							<label>Mail Adresiniz</label>
+							<input type="email" class="form-control newstler-input" id="exampleInputEmail1" placeholder="blank@blank.com">
+							<button class="btn btn-default btn-red btn-sm">Kayıt Ol</button>
+						</div>
+					</form>
+				</div>
+			</div><!--footer newsletter widget-->
 		</div>
 		<div class="spacer"></div>
 	</div>
@@ -81,14 +81,14 @@
 		<div class="row">
 			<div class="col-md-9">
 				<ul class="footermenu"><!--footer nav-->
-					<li><a href="index-1.htm">Home</a></li>
+					<!-- <li><a href="index-1.htm">Home</a></li>
 					<li><a href="cart.htm">My Cart</a></li>
 					<li><a href="checkout.htm">Checkout</a></li>
 					<li><a href="order.htm">Completed Orders</a></li>
-					<li><a href="contact.htm">Contact us</a></li>
+					<li><a href="contact.htm">Contact us</a></li> -->
 				</ul><!--footer nav-->
 				<div class="f-credit">&copy;<?php echo $ayarcek['ayar_author'] ?> <a href="#">E ticaret Sitesi</a></div>
-				<a href="">
+				<!-- <a href="">
 					<div class="payment visa"></div>
 				</a>
 				<a href="">
@@ -99,10 +99,10 @@
 				</a>
 				<a href="">
 					<div class="payment nh"></div>
-				</a>
+				</a> -->
 			</div>
 			<div class="col-md-3"><!--footer Share-->
-				<div class="followon">Follow us on</div>
+				<div class="followon">Bizi Takip Edin</div>
 				<div class="fsoc">
 					<a href="http://<?php echo $ayarcek['ayar_twitter']; ?>" class="ftwitter">twitter</a>
 					<a href="http://<?php echo $ayarcek['ayar_facebook']; ?>" class="ffacebook">facebook</a>
