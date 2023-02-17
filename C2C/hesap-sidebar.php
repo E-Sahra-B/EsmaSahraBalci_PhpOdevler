aa205efd817d22e6a2cce44ec2f8d6b81ae4387b
E-Sahra-B <bendenizsahra@gmail.com>
2023-02-17 17:29:40 +0300

    MagazaBasvuru

--- C2C/hesap-sidebar.php
+++ C2C/hesap-sidebar.php
@@ -1,9 +1,9 @@
 <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
     <ul class="settings-title">
-        <li class="active"><a href="hesabim">Hesap Bilgilerim</a></li>
+        <li class="active"><a href="javascript:void(0)"><b>ÜYE İŞLEMLERİ</b></a></li>
+        <li><a href="magaza-basvuru">Mağaza Başvuru</a></li>
+        <li><a href="hesabim">Hesap Bilgilerim</a></li>
         <li><a href="adres-bilgileri">Adres Bilgilerim</a></li>
-        <li><a href="#Badges">Badges</a></li>
-        <li><a href="#E-mail">E-mail Settings</a></li>
-        <li><a href="#Social">Social Network</a></li>
+        <li><a href="sifre-guncelle">Şifre Guncelle</a></li>
     </ul>
 </div>
