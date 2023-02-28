3ffbdffc5cb6c0d5352ffd819b999a9f03b995e1
E-Sahra-B <bendenizsahra@gmail.com>
2023-03-01 00:52:40 +0300

    KategorilerSayfasi

---
+++ C2C/search.php
@@ -0,0 +1,17 @@
+<!-- Main Banner 1 Area Start Here -->
+<div class="inner-banner-area">
+    <div class="container">
+        <div class="inner-banner-wrapper">
+            <p>Aradığınız ürün yada hizmetin adını giriniz...</p>
+            <div class="banner-search-area input-group">
+                <input class="form-control" required="" minlength="3" name="searchkeyword" placeholder="aradığınız ürün adını giriniz . . ." type="text">
+                <span class="input-group-addon">
+                    <button type="submit" name="searchsayfa">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Main Banner 1 Area End Here -->
