f9e069a147eab7b51a8209a459882697f7606b77
E-Sahra-B <bendenizsahra@gmail.com>
2023-04-30 15:54:46 +0300

    Bakimdayiz

--- C2C/header.php
+++ C2C/header.php
@@ -114,6 +114,11 @@ if (isset($_SESSION['userkullanici_sonzaman'])) {
 </head>
 
 <body>
+    <?php
+    if ($ayarcek['ayar_bakim'] == 1) {
+        exit("Şuan Bakımdayız...");
+    }
+    ?>
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
